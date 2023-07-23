c9a8e5de33373f33d5b05c5181f277db16d6f66a
Likhith Raju <likhithgraju@gmail.com>
2023-07-23 12:11:00 +0530

    jwt -> BE

--- hybris/api/user/login.js
+++ hybris/api/user/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const UserModel = require("./UserModel");
 
 var router = express.Router();
@@ -13,11 +14,13 @@ router.post("/", async (req, res) => {
       res.status(404).json({ message: "User doesn't exist, Please Register" });
     else {
       const passwordMatch = await bcrypt.compare(password, user?.password);
-      if (passwordMatch)
-        res
-          .status(200)
-          .json({ message: "Login Successfull", userId: user?._id });
-      else res.status(401).json({ message: "Incorrect username or password" });
+      if (passwordMatch) {
+        const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+          expiresIn: "1h",
+        });
+        res.status(200).json({ message: "Login Successfull", token });
+      } else
+        res.status(401).json({ message: "Incorrect username or password" });
     }
   } catch (err) {
     res.status(500).json({ message: "Error during login" });
