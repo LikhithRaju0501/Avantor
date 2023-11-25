31b4becf680c6300d86b94785bb90e45e88602ce
LikhithRajuibm <likhith.raju1@ibm.com>
2023-11-25 22:44:19 +0530

    Refactor

--- storefront/src/components/CxSpinner/index.js
+++ storefront/src/components/CxSpinner/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Spinner } from "react-bootstrap";
 import "./index.css";
 
-const CxSpinner = ({ variant }) => {
+const CxSpinner = ({ variant = "light" }) => {
   return (
     <div className="overlay d-flex flex-column">
-      <Spinner animation="border" role="status" variant={variant || "light"} />
+      <Spinner animation="border" role="status" variant={variant} />
       <div className="loading-header mt-2">Loading...</div>
     </div>
   );
