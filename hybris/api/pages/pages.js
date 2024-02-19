58be5c16c26153c8905f48feff6d895835f38e69
LikhithRajuibm <likhith.raju1@ibm.com>
2024-02-19 18:28:17 +0530

    BE | Priority in CMS Components

--- hybris/api/pages/pages.js
+++ hybris/api/pages/pages.js
@@ -27,19 +27,35 @@ router.post("/add-page", async (req, res) => {
     const { user, pathname, component } = req?.body;
     if (user?.name === "Likki" && user?.safeWord === "Span") {
       try {
-        const currentPage = await pagesModel.findOne({
-          pathname,
-        });
+        const currentPage = await pagesModel
+          .findOne({
+            pathname,
+          })
+          .lean();
         try {
           if (currentPage) {
-            const { _id } = currentPage;
+            const { _id, components: currentComponents } = currentPage;
+            if (
+              currentComponents?.some(
+                (currentComponent) =>
+                  currentComponent?.priority === component?.priority
+              )
+            ) {
+              return res.status(201).json({
+                message:
+                  "Priority already exists, please change the priorities to adjust the order",
+              });
+            }
+            const newComponents = [
+              ...currentComponents,
+              { ...component },
+            ]?.sort((a, b) => a?.priority - b?.priority);
+
             result = await pagesModel.findOneAndUpdate(
               { _id },
               {
-                $push: {
-                  components: {
-                    ...component,
-                  },
+                $set: {
+                  components: [...newComponents],
                 },
               },
               { new: true }
@@ -123,6 +139,78 @@ router.delete("/", async (req, res) => {
           } else {
             return res.status(404).json({
               message: "CMS Page not Found",
+            });
+          }
+        } catch (error) {
+          console.error(error);
+          return res.status(500).json({
+            message: "Something Went Wrong",
+          });
+        }
+      } catch (e) {
+        console.error(error);
+        return res.status(500).json({
+          message: "Something Went Wrong",
+        });
+      }
+    } else {
+      return res.status(401).json({
+        message:
+          "You are not authorized to perform this action, Please contact the Admin.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const {
+      user,
+      pathname,
+      componentId,
+      component: updatedComponent,
+    } = req?.body;
+    if (user?.name === "Likki" && user?.safeWord === "Span") {
+      try {
+        const currentPage = await pagesModel.findOne({
+          pathname,
+        });
+        try {
+          if (currentPage) {
+            const { _id: currentPageId, components: currentComponents } =
+              currentPage;
+            const index = currentComponents.findIndex(
+              (component) => String(component?.id) === String(componentId)
+            );
+            let newComponents = [...currentComponents];
+            if (index !== -1) {
+              newComponents[index] = { ...updatedComponent };
+              result = await pagesModel.findOneAndUpdate(
+                { _id: currentPageId },
+                {
+                  $set: {
+                    components: [...newComponents],
+                  },
+                },
+                { new: true }
+              );
+              return res.status(201).json({
+                result,
+              });
+            } else {
+              return res.status(404).json({
+                message: "No Such Component with the ID found.",
+              });
+            }
+          } else {
+            return res.status(404).json({
+              message: "No Such Component found.",
             });
           }
         } catch (error) {
